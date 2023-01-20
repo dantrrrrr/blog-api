69285041ace2b88533f7f8459dd44efb29fc02ce
dantr <dantruongeth@gmail.com>
2023-01-20 13:44:58 +0700

    fix img show

--- index.js
+++ index.js
@@ -2,38 +2,57 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const route=require('./routes');
-const cors =require('cors');
-const multer =require('multer');
+const route = require('./routes');
+const cors = require('cors');
+const multer = require('multer');
 
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static(__dirname+'/public/'))
 
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log("connect to Database Successfully"))
+    .then(() => console.log("connect to Database Successfully"))
     .catch(err => console.log(err))
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"public/images")
+    destination: (req, file, cb) => {
+        cb(null, "public/images")
 
-    },filename:(req,file,cb)=>{
+    }, filename: (req, file, cb) => {
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         // cb(null, file.originalname.split('.')[0] + Math.round(Math.random()*1E6)+'.'+file.originalname.split('.')[1])
         // console.log(file)
-        cb(null,req.body.name)
-       
+        cb(null, req.body.name)
+
     }
 });
-const upload =multer({storage:storage}) 
-app.post('/api/upload',upload.single('file'),(req,res)=>{
-    res.status(200).json("file has been uploaded");
-    console.log("Upload ok")
+const maxSize = 2 * 1024 * 1024;
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: maxSize }
+}).single("file")
+app.post('/api/upload', upload, async (req, res) => {
+    try {
+        if (req.file == undefined) {
+            return res.status(400).send({ message: "Please upload a file!" });
+
+        }
+
+        res.status(200).json({
+            message: "Uploaded the file successfully: " + req.file.originalname,
+        });
+        console.log("Upload ok")
+    } catch (error) {
+        res.status(500).json({
+            message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+        })
+    }
+
 });
 
 
